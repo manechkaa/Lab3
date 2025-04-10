5b2d3c2b00cc31d94fd790511a8c28cb57c5c4ec
manechkaa <mariatestova5@gmail.com>
2025-04-11 00:30:56 +0300

    make all tasks

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled6")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Lab3")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion2024.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- main.cpp
+++ main.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
+#include <memory>
 
-void NewMemory() {
-    while(1) {
+void Task1() {
+    for (int i = 0; i < 1e8 * 5; i++) {
         int *a = new int(1);
+        //delete a;
+    }
+}
+
+void Task2() {
+    for (int i = 0; i < 1e8 * 3; i++) {
+        auto safePtr = std::make_unique<int>(1);
+    }
+}
+
+void fortask3(std::shared_ptr<int> &a, int p = 0) {
+    if (p == 200) return;
+    auto copy = a;
+    std::cout << "Use count: " << a.use_count() << "\n";
+    fortask3(copy, p + 1);
+    std::cout << "Use count: " << a.use_count() << "\n";
+}
+void Task3() {
+    auto shr = std::make_shared<int>(100);
+    std::cout << "Use count: " << shr.use_count() << std::endl;
+    fortask3(shr);
+    std::cout << "Use count: " << shr.use_count() << std::endl;
+}
+
+void Task4() {
+    auto shr = std::make_shared<int>(100);
+    std::weak_ptr<int> weak = shr;
+
+    if (auto locked = weak.lock()) {
+        std::cout << "alive: " << *locked << std::endl;
+    } else {
+        std::cout << "dead" << std::endl;
+    }
+
+    shr.reset();
+
+    if (auto locked = weak.lock()) {
+        std::cout << "alive: " << *locked << std::endl;
+    } else {
+        std::cout << "dead" << std::endl;  // Выведется это
     }
 }
 
 int main() {
-    NewMemory();
+    Task4();
+
     return 0;
 }
